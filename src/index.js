a3963518af855a568543e85143a7a71af7c5da10
kanitsharma <kanitshrm09@gmail.com>
2019-04-01 03:38:23 +0530

    Added nodejs implementation

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-function getWorkerFunctionString(fn, ...args) {
+function getJSWorker(fn, ...args) {
   return `
     const self = this;
     const a = (${fn})(${args.join(', ')})
@@ -10,16 +10,40 @@ function getWorkerFunctionString(fn, ...args) {
   `;
 }
 
+function getNodeWorker(fn, ...args) {
+  return `
+    const { parentPort } = require('worker_threads');
+
+    const a = (${fn})(${args.join(', ')})
+    if (Promise.resolve(a) == a) {
+      a.then(parentPort.postMessage)
+    } else {
+      parentPort.postMessage(a);
+    }
+  `;
+}
+
 function createWorker(fn, ...args) {
-  const functionString = getWorkerFunctionString(fn, ...args);
+  if (typeof window === 'undefined') {
+    const functionString = getNodeWorker(fn, ...args);
+    const { Worker } = require('worker_threads'); // eslint-disable-line
+
+    return new Promise(resolve => {
+      const worker = new Worker(functionString, { eval: true });
+      worker.on('message', resolve);
+    });
+  }
+
+  const functionString = getJSWorker(fn, ...args);
 
   const blob = new Blob([functionString.replace('"use strict";', '')]); // eslint-disable-line no-undef
 
+  // eslint-disable-next-line no-undef
   const blobURL = (window.URL ? URL : webkitURL).createObjectURL(blob, {
-    // eslint-disable-line no-undef
     type: 'application/javascript; charset=utf-8',
   });
 
+  // eslint-disable-next-line no-undef
   const worker = new Worker(blobURL);
 
   return new Promise(resolve => {
@@ -31,10 +55,12 @@ function createWorker(fn, ...args) {
 
 createWorker(
   (a, b) => {
-    let start = Date.now();
-    while (Date.now() - start < 1000);
+    const start = Date.now();
+    while (Date.now() - start < 2000);
     return a + b;
   },
   3,
   9,
 ).then(console.log);
+
+module.exports = createWorker;
