74c42e6699bbbf4841fa1e4021fcea42c15d07b4
kanitsharma <kanitshrm09@gmail.com>
2019-04-01 13:10:47 +0530

    Added error handling

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 function getJSWorker(fn, ...args) {
   return `
     const self = this;
-    const a = (${fn})(${args.join(', ')})
-    if (Promise.resolve(a) == a) {
-      a.then(self.postMessage)
-    } else {
-      self.postMessage(a);
+
+    try {
+      const a = (${fn})(${args.join(', ')})
+      if (Promise.resolve(a) == a) {
+        a
+          .then(val => self.postMessage({ type: 'SUCCESS', payload: val }))
+          .catch(err => self.postMessage({ type: 'ERROR', payload: err }))
+      } else {
+        self.postMessage({ type: 'SUCCESS', payload: a });
+      }
+    } catch (err) {
+      self.postMessage({ type: 'ERROR', payload: err })
     }
   `;
 }
@@ -14,13 +21,28 @@ function getNodeWorker(fn, ...args) {
   return `
     const { parentPort } = require('worker_threads');
 
-    const a = (${fn})(${args.join(', ')})
-    if (Promise.resolve(a) == a) {
-      a.then(parentPort.postMessage)
-    } else {
-      parentPort.postMessage(a);
+    try {
+      const a = (${fn})(${args.join(', ')})
+      if (Promise.resolve(a) == a) {
+        a
+          .then(val => parentPort.postMessage({ type: 'SUCCESS', payload: val }))
+          .catch(err => parentPort.postMessage({ type: 'ERROR', payload: err }))
+      } else {
+        parentPort.postMessage({ type: 'SUCCESS', payload: a });
+      }
+    } catch (err) {
+      parentPort.postMessage({ type: 'ERROR', payload: err })
     }
   `;
+}
+
+function reducer(action, resolve, reject) {
+  switch (action.type) {
+    case 'SUCCESS':
+      return resolve(action.payload);
+    case 'ERROR':
+      return reject(action.payload);
+  }
 }
 
 function workinator(fn, ...args) {
@@ -28,9 +50,11 @@ function workinator(fn, ...args) {
     const functionString = getNodeWorker(fn, ...args);
     const { Worker } = eval('require')('worker_threads'); // eslint-disable-line
 
-    return new Promise(resolve => {
+    return new Promise((resolve, reject) => {
       const worker = new Worker(functionString, { eval: true });
-      worker.on('message', resolve);
+      worker.on('message', action => {
+        reducer(action, resolve, reject);
+      });
     });
   }
 
@@ -46,11 +70,11 @@ function workinator(fn, ...args) {
   // eslint-disable-next-line no-undef
   const worker = new Worker(blobURL);
 
-  return new Promise(resolve => {
-    worker.onmessage = e => {
-      resolve(e.data);
+  return new Promise((resolve, reject) => {
+    worker.onmessage = ({ data: action }) => {
+      reducer(action, resolve, reject);
     };
   });
 }
 
-export default workinator
+export default workinator;
